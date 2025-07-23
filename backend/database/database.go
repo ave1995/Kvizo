2ae5d657dfa6322560ba38510aceeb47abc187e9
ave <veselya95@gmail.com>
2025-07-23 21:11:10 +0200

    load config

--- backend/database/database.go
+++ backend/database/database.go
@@ -11,19 +11,38 @@ import (
 	"gorm.io/gorm"
 )
 
-func NewDatabaseConnection() (*gorm.DB, error) {
+type DBConfig struct {
+	Host     string
+	User     string
+	Password string
+	DBName   string
+	Port     string
+}
+
+func loadDBConfig() (*DBConfig, error) {
 	godotenv.Load()
 	viper.AutomaticEnv()
 
-	host := viper.GetString("DB_HOST")
-	user := viper.GetString("DB_USER")
-	password := viper.GetString("DB_PASSWORD")
-	dbname := viper.GetString("DB_NAME")
-	port := viper.GetString("DB_PORT")
+	var cfg DBConfig
+
+	cfg.Host = viper.GetString("DB_HOST")
+	cfg.User = viper.GetString("DB_USER")
+	cfg.Password = viper.GetString("DB_PASSWORD")
+	cfg.DBName = viper.GetString("DB_NAME")
+	cfg.Port = viper.GetString("DB_PORT")
+
+	return &cfg, nil
+}
+
+func NewDatabaseConnection() (*gorm.DB, error) {
+	cfg, err := loadDBConfig()
+	if err != nil {
+		return nil, err
+	}
 
 	dsn := fmt.Sprintf(
 		"host=%s user=%s password=%s dbname=%s port=%s",
-		host, user, password, dbname, port,
+		cfg.Host, cfg.User, cfg.Password, cfg.DBName, cfg.Port,
 	)
 
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
